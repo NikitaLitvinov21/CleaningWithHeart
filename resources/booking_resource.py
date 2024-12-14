cc324201ca69deb425daffeda75d99619d67ccb5
AlexNottaBen <alex.nottaben@gmail.com>
2024-12-14 22:43:07 +0200

    Repleced deprecated constr to StringConstraints.

--- resources/booking_resource.py
+++ resources/booking_resource.py
@@ -1,3 +1,4 @@
+from flask import Response
 from flask_restful import Resource
 
 from schemes.booking_scheme import BookingScheme
@@ -7,6 +8,10 @@ from utils.validate_by_scheme import validate_by_scheme
 class BookingResource(Resource):
 
     @validate_by_scheme(BookingScheme)
-    def post(self, booking: BookingScheme) -> dict:
+    def post(self, booking: BookingScheme) -> Response:
         print(booking)
-        return {"message": "Flask comes alive!"}
+        return Response(
+            status=201,
+            content_type="application/json",
+            response='{"message": "Your booking has been accepted!"}',
+        )
--- schemes/booking_scheme.py
+++ schemes/booking_scheme.py
@@ -1,31 +1,47 @@
 from datetime import datetime
+from typing import Annotated
 
-from pydantic import constr
+from pydantic import StringConstraints
 
 from schemes.scheme import Scheme
 
-str_regexp = "^[^\d\!\@\#\$\%\^\&\*\=\+\~\/\+\;\:\?\{\}]*$"
+str_regexp = r"^[^\d\!\@\#\$\%\^\&\*\=\+\~\/\+\;\:\?\{\}]*$"
 
 
 class BookingScheme(Scheme):
 
-    first_name: constr(
-        min_length=1,
-        max_length=30,
-        regex=str_regexp,
-    )
-    last_name: constr(
-        min_length=1,
-        max_length=30,
-        regex=str_regexp,
-    )
-    phone_number: constr(
-        min_length=11,
-        max_length=11,
-        regex="^1\d{10}$",
-    )
-    street: constr(
-        min_length=1,
-        max_length=30,
-    )
+    first_name: Annotated[
+        str,
+        StringConstraints(
+            min_length=1,
+            max_length=30,
+            pattern=str_regexp,
+            strip_whitespace=True,
+        ),
+    ]
+    last_name: Annotated[
+        str,
+        StringConstraints(
+            min_length=1,
+            max_length=30,
+            pattern=str_regexp,
+            strip_whitespace=True,
+        ),
+    ]
+    phone_number: Annotated[
+        str,
+        StringConstraints(
+            min_length=11,
+            max_length=11,
+            pattern=r"^1\d{10}$",
+        ),
+    ]
+    street: Annotated[
+        str,
+        StringConstraints(
+            min_length=1,
+            max_length=30,
+            strip_whitespace=True,
+        ),
+    ]
     datetime_local: datetime
