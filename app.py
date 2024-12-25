75f582aeacb63664d786aeb90f6ae383620b8cc2
Mykyta Lytvynov <mlytvynov@lacosolutions.net>
2024-12-25 18:28:46 +0200

    add login and booking page

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from database.connector import create_tables
 from resources.booking_resource import BookingResource
 from views.index_view import IndexView
 from views.login_view import LoginView
+from views.booking_view import BookingView
 
 load_dotenv()
 create_tables()
@@ -19,6 +20,7 @@ api = Api(app=app)
 
 app.add_url_rule(rule="/", view_func=IndexView.as_view(name="index"))
 app.add_url_rule(rule="/login", view_func=LoginView.as_view(name="login"))
+app.add_url_rule(rule="/booking", view_func=BookingView.as_view(name="booking"))
 api.add_resource(BookingResource, "/api/booking")
 
 
---
+++ static/js/booking.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("bookingForm");
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const currentDateTime = `${year}-${month}-${day} ${hours}:${minutes}`;
+    const dateTimeInput = form.querySelector('input[name="datetime_local"]');
+    dateTimeInput.min = currentDateTime;
+    
+});
+
+function saveBooking() {
+    const form = document.getElementById("bookingForm");
+
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
+
+    const formData = new FormData(form);
+
+    const body = {};
+
+    for (const [key, value] of formData.entries()) {
+        body[key] = value;
+    }
+
+    console.log(body);
+
+    fetch("/api/booking", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(body),
+    })
+        .then(response => {
+            if (response.ok) {
+                // window.location.href = "/";
+                return response.json();
+            } else {
+                throw new Error(response.status);
+            }
+        })
+        .then(responseData => {
+            console.log(responseData);
+            return responseData;
+        })
+        .catch(error => {
+            console.error("Error requesting - " + error);
+        });
+}
